ac96de7f526c8fb2bb628b6802ef1cf51cfd2dd8
JeongSubi <jsb97613@naver.com>
2023-10-26 12:12:08 +0900

    feature: jwt module

---
+++ libs/jwt/src/jwt.service.ts
@@ -0,0 +1,29 @@
+import {Inject, Injectable, UnauthorizedException} from '@nestjs/common'
+import jwt, {Secret, SignOptions, VerifyOptions} from 'jsonwebtoken'
+
+@Injectable()
+export class JwtService {
+  private privateKey: string
+  private publicKey: string
+
+  constructor(@Inject('CONFIG_OPTIONS') private options) {
+    this.publicKey = options.publicKey
+    this.privateKey = options.privateKey
+  }
+
+  createToken(payload: any, options: SignOptions, secret: Secret = this.privateKey): string {
+    try {
+      return jwt.sign(payload, secret, options)
+    } catch (e) {
+      throw e
+    }
+  }
+
+  decodeToken(token: string, options: VerifyOptions, secret: Secret = this.publicKey): any {
+    try {
+      return jwt.verify(token, secret, options)
+    } catch (e) {
+      throw new UnauthorizedException('invalid_token')
+    }
+  }
+}
