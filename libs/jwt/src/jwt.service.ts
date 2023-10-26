9949e700eb138ef327c41b6050a0e1161e3e7a2d
JeongSubi <jsb97613@naver.com>
2023-10-26 12:14:37 +0900

    refactor: module config 수정

--- libs/jwt/src/jwt.service.ts
+++ libs/jwt/src/jwt.service.ts
@@ -1,29 +1,29 @@
-import {Inject, Injectable, UnauthorizedException} from '@nestjs/common'
-import jwt, {Secret, SignOptions, VerifyOptions} from 'jsonwebtoken'
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import jwt, { Secret, SignOptions, VerifyOptions } from 'jsonwebtoken';
 
 @Injectable()
 export class JwtService {
-  private privateKey: string
-  private publicKey: string
+  private privateKey: string;
+  private publicKey: string;
 
   constructor(@Inject('CONFIG_OPTIONS') private options) {
-    this.publicKey = options.publicKey
-    this.privateKey = options.privateKey
+    this.publicKey = options.publicKey;
+    this.privateKey = options.privateKey;
   }
 
   createToken(payload: any, options: SignOptions, secret: Secret = this.privateKey): string {
     try {
-      return jwt.sign(payload, secret, options)
-    } catch (e) {
-      throw e
+      return jwt.sign(payload, secret, options);
+    } catch (error) {
+      throw error;
     }
   }
 
   decodeToken(token: string, options: VerifyOptions, secret: Secret = this.publicKey): any {
     try {
-      return jwt.verify(token, secret, options)
-    } catch (e) {
-      throw new UnauthorizedException('invalid_token')
+      return jwt.verify(token, secret, options);
+    } catch (error) {
+      throw new UnauthorizedException('invalid_token');
     }
   }
 }
